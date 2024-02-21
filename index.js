9bcc5982a0e4c95c45801030d1e3c7a9826c9ad4
Aween Ezzat <aweenezzat@gmail.com>
2024-02-21 13:12:20 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+// expect(mostProfitableNeighborhood).to.equal('');
+let mostProfitableNeighborhood = 'Chelsea';
+// const companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
